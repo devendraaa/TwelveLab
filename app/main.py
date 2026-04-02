8fd3554d5b71ea48b058e75fb8a398124f1393d8
devendraaa <devendrachavan31@gmail.com>
2026-04-02 20:57:22 +0530

    code changes

--- app/main.py
+++ app/main.py
@@ -351,8 +351,8 @@ def health():
         "time":     int(time.time()),
     }
 
-# handler = app
+handler = app
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
